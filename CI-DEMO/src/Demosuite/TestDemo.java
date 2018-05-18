54e0dc45c4adddecbd45de04adaaf57b7e3c5661
arjun <arjun@Arjun>
2018-05-18 17:08:50 +0530

    Commit1...

---
+++ CI-DEMO/src/Demosuite/TestDemo.java
@@ -0,0 +1,24 @@
+package Demosuite;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1 started");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test2 started");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test3 started");
+	}
+
+
+
+}
